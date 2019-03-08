c5eed712451d5b90092ca5f27ea20826c9e140f0
Sharif Ahmed <shiarif@gmail.com>
2019-03-07 23:40:03 -0500

    Add files via upload

---
+++ faceLandmarks.py
@@ -0,0 +1,118 @@
+# import the necessary packages
+from scipy.spatial import distance as dist
+from imutils import face_utils
+import numpy as np
+import argparse
+import imutils
+import dlib
+import cv2
+
+def eye_aspect_ratio(eye):
+	# compute the euclidean distances between the two sets of
+	# vertical eye landmarks (x, y)-coordinates
+	A = dist.euclidean(eye[1], eye[5])
+	B = dist.euclidean(eye[2], eye[4])
+ 
+	# compute the euclidean distance between the horizontal
+	# eye landmark (x, y)-coordinates
+	C = dist.euclidean(eye[0], eye[3])
+ 
+	# compute the eye aspect ratio
+	ear = (A + B) / (2.0 * C)
+ 
+	# return the eye aspect ratio
+	return ear
+
+def rect_to_bb(rect):
+	# take a bounding predicted by dlib and convert it
+	# to the format (x, y, w, h) as we would normally do
+	# with OpenCV
+	x = rect.left()
+	y = rect.top()
+	w = rect.right() - x
+	h = rect.bottom() - y
+ 
+	# return a tuple of (x, y, w, h)
+	return (x, y, w, h)
+
+def shape_to_np(shape, dtype="int"):
+	# initialize the list of (x, y)-coordinates
+	coords = np.zeros((68, 2), dtype=dtype)
+ 
+	# loop over the 68 facial landmarks and convert them
+	# to a 2-tuple of (x, y)-coordinates
+	for i in range(0, 68):
+		coords[i] = (shape.part(i).x, shape.part(i).y)
+ 
+	# return the list of (x, y)-coordinates
+	return coords
+
+EYE_AR_THRESH = 0.21
+TOTAL=0
+# construct the argument parser and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-p", "--shape-predictor", required=True,
+	help="path to facial landmark predictor")
+ap.add_argument("-i", "--image", required=True,
+	help="path to input image")
+args = vars(ap.parse_args())
+
+# initialize dlib's face detector (HOG-based) and then create
+# the facial landmark predictor
+detector = dlib.get_frontal_face_detector()
+predictor = dlib.shape_predictor(args["shape_predictor"])
+(lStart, lEnd) = face_utils.FACIAL_LANDMARKS_IDXS["left_eye"]
+(rStart, rEnd) = face_utils.FACIAL_LANDMARKS_IDXS["right_eye"]
+
+# load the input image, resize it, and convert it to grayscale
+image = cv2.imread(args["image"])
+image = imutils.resize(image, width=700)
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+ 
+# detect faces in the grayscale image
+rects = detector(gray, 1)
+
+# loop over the face detections
+for (i, rect) in enumerate(rects):
+	# determine the facial landmarks for the face region, then
+	# convert the facial landmark (x, y)-coordinates to a NumPy
+	# array
+	shape = predictor(gray, rect)
+	shape = shape_to_np(shape)
+    
+	leftEye = shape[lStart:lEnd]
+	rightEye = shape[rStart:rEnd]
+	leftEAR = eye_aspect_ratio(leftEye)
+	rightEAR = eye_aspect_ratio(rightEye)
+
+    #if leftEAR < EYE_AR_THRESH:
+    #    LCOUNTER += 1
+    #if rightEAR < EYE_AR_THRESH:
+    #    RCOUNTER += 1
+    #print(leftEye[0][0])
+    #mouse.position = (leftEye[0][0], leftEye[0][1])
+    # average the eye aspect ratio together for both eyes
+	ear = (leftEAR + rightEAR) / 2.0
+	if ear <= EYE_AR_THRESH:
+		TOTAL += 1    
+	cv2.putText(image, "Blinks: {}".format(TOTAL), (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+	cv2.putText(image, "EAR: {:.2f}".format(ear), (300, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+ 
+	# convert dlib's rectangle to a OpenCV-style bounding box
+	# [i.e., (x, y, w, h)], then draw the face bounding box
+	(x, y, w, h) = rect_to_bb(rect)
+	cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
+ 
+	# show the face number
+	cv2.putText(image, "Face #{}".format(i + 1), (x - 10, y - 10),
+		cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+ 
+	# loop over the (x, y)-coordinates for the facial landmarks
+	# and draw them on the image
+	for (x, y) in shape:
+		cv2.circle(image, (x, y), 1, (0, 0, 255), -1)
+ 
+# show the output image with the face detections + facial landmarks
+cv2.imshow("Output", image)
+cv2.waitKey(0)
+#python faceLandmarks.py --shape-predictor ~/Downloads/shape_predictor_68_face_landmarks.dat --image ~/Downloads/openeye.jpg 
